9d875894556d15cf0a5533b66a9a8a0a6ecd6a88
PhyoPyae94 <kirudozack@gmail.com>
2019-05-04 18:37:48 +0630

    Creating Post Form

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -42,7 +42,7 @@
                         @method('DELETE')
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="deleteModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
